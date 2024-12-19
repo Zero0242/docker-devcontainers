b56587a10427801c6d2b9794e25e0099665a7879
Zero0242 <pedrocorream66@gmail.com>
2024-12-19 10:11:29 -0300

    [KOTLIN]: app de spring boot

---
+++ spring_kotlin_app/src/main/kotlin/com/springsegundoapi/demo/SegundoApiApplication.kt
@@ -0,0 +1,11 @@
+package com.springsegundoapi.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SegundoApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<SegundoApiApplication>(*args)
+}
---
+++ spring_kotlin_app/src/test/kotlin/com/springsegundoapi/demo/SegundoApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.springsegundoapi.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SegundoApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
