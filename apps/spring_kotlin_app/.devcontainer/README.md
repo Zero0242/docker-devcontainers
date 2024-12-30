5ad09d2901638d2edf24edf7a709a3fe72985f54
Zero0242 <pedrocorream66@gmail.com>
2024-12-30 13:01:50 -0300

    [KOTLIN]: devcontainer config

---
+++ apps/spring_kotlin_app/.devcontainer/README.md
@@ -0,0 +1,10 @@
+# Kotlin
+
+El entorno ya contiene SDKMan para realizar la instalacion, los pasos necesarios son.
+
+1. Abrir la terminal y ejecutar
+
+```sh
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+sdk install kotlin
+```
--- apps/spring_kotlin_app/src/main/kotlin/com/springsegundoapi/demo/SegundoApiApplication.kt
+++ apps/spring_kotlin_app/src/main/kotlin/com/springsegundoapi/demo/SegundoApiApplication.kt
@@ -3,9 +3,8 @@ package com.springsegundoapi.demo
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
-class SegundoApiApplication
+@SpringBootApplication class SegundoApiApplication
 
 fun main(args: Array<String>) {
-	runApplication<SegundoApiApplication>(*args)
+    runApplication<SegundoApiApplication>(*args)
 }
